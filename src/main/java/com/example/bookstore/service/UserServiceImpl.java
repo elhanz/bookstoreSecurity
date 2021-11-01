32752cddec099a48d5d230dc1c2550d007737338
elhanz <201338@astanait.edu.kz>
2021-11-02 02:21:00 +0600

    SecurityConfig

--- src/main/java/com/example/bookstore/service/UserServiceImpl.java
+++ src/main/java/com/example/bookstore/service/UserServiceImpl.java
@@ -6,15 +6,35 @@ import com.example.bookstore.repositories.RoleRepository;
 import com.example.bookstore.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 @Service @RequiredArgsConstructor @Transactional @Slf4j
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRep;
     private final RoleRepository roleRep;
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+      User user= userRep.findByEmail(email);
+      if(user==null){
+          log.error("User not found in the database");
+          throw new UsernameNotFoundException("User not found in the database");
+      } else {
+          log.info("User found:{}",email);
+      }
+        Collection<SimpleGrantedAuthority> authorities=new ArrayList<>();
+      user.getRoles().forEach(role -> {authorities.add(new SimpleGrantedAuthority(role.getName()));
+      });
+      return new org.springframework.security.core.userdetails.User(user.getEmail(),user.getPassword(),authorities);
+    }
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getEmail());
@@ -46,4 +66,6 @@ public class UserServiceImpl implements UserService {
         log.info("Fetching all users");
         return userRep.findAll();
     }
+
+
 }
