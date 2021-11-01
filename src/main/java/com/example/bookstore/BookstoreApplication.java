e2071e10a5bcaced2caebaa13fe5ad08d0d18a87
elhanz <201338@astanait.edu.kz>
2021-11-02 00:53:50 +0600

    .

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,12 +1,30 @@
 package com.example.bookstore;
 
+import com.example.bookstore.entities.Role;
+import com.example.bookstore.entities.User;
+import com.example.bookstore.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class BookstoreApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
+CommandLineRunner run (UserService userService){
+		return args ->{
+		userService.saveRole(new Role(null,"ROLE_USER"));
+			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
 
+			userService.saveUser(new User(null,"Yelkhan Zhumatayev","elhanz@mail.ru","1555",new ArrayList<>()));
+			userService.saveUser(new User(null,"Kanat Berkinbayev","kanat@mail.ru","1337",new ArrayList<>()));
+			userService.saveUser(new User(null,"Daniyar Myrzasary","kaban@mail.ru","322",new ArrayList<>()));
+			userService.saveUser(new User(null,"Talgar Nurdinov","vp@mail.ru","228",new ArrayList<>()));
+		};
+}
 }
--- src/main/java/com/example/bookstore/controllers/UserController.java
+++ src/main/java/com/example/bookstore/controllers/UserController.java
@@ -1,14 +1,17 @@
 package com.example.bookstore.controllers;
 
+import com.example.bookstore.entities.Role;
 import com.example.bookstore.entities.User;
 import com.example.bookstore.service.UserService;
 
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,6 +23,25 @@ public class UserController {
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user) {
+        URI uri= URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role>saveRole(@RequestBody Role role) {
+        URI uri= URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getEmail(), form.getRoleName());
+    return ResponseEntity.ok().build();
+    }
 
-
+}
+@Data
+class RoleToUserForm{
+    private String email;
+    private String roleName;
 }
