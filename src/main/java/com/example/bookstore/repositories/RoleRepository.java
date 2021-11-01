a97c5f11839559d78198708b740a21da4c64f347
elhanz <201338@astanait.edu.kz>
2021-11-01 23:52:05 +0600

    begin

---
+++ src/main/java/com/example/bookstore/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.bookstore.repositories;
+
+import com.example.bookstore.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
