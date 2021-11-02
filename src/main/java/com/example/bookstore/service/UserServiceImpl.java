a2d12bfa4ee0e494ad8b37b0e9e461f18c553b39
elhanz <201338@astanait.edu.kz>
2021-11-02 14:57:14 +0600

    authorization authentication

--- src/main/java/com/example/bookstore/service/UserServiceImpl.java
+++ src/main/java/com/example/bookstore/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -20,7 +21,7 @@ import java.util.List;
 public class UserServiceImpl implements UserService, UserDetailsService {
     private final UserRepository userRep;
     private final RoleRepository roleRep;
-
+private final PasswordEncoder passwordEncoder;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
       User user= userRep.findByEmail(email);
@@ -38,6 +39,7 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database", user.getEmail());
+       user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRep.save(user);
     }
 
